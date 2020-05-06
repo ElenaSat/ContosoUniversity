c0be1cf8ce560d7a3417d55e6755327bad0147d0
ElenaSat <37122490+ElenaSat@users.noreply.github.com>
2020-05-06 16:48:15 -0500

    CRUD-OfficeAssignment

---
+++ ContosoUniversity/Controllers/OfficeAssignmentsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.Services;
+using AutoMapper;
+using ContosoUniversity.DTOs;
+
+namespace ContosoUniversity.Controllers
+{
+    public class OfficeAssignmentsController : Controller
+    {
+        private readonly IOfficeAssignmentService _assignmentService;
+        private readonly IInstructorService _instructorService;
+        private readonly IMapper _mapper;
+
+        public OfficeAssignmentsController(IOfficeAssignmentService officeAssignmentService, IMapper mapper, IInstructorService instructorService)
+        {
+            _assignmentService = officeAssignmentService;
+            _mapper = mapper;
+            _instructorService = instructorService;
+        }
+
+        // GET: OfficeAssignments
+        public async Task<IActionResult> Index()
+        {
+            var data = await _assignmentService.GetAll();
+            var listOfficeAssignments = data.Select(x => _mapper.Map<OfficeAssignmentDTO>(x)).ToList();
+            return View(listOfficeAssignments);
+        }
+
+        // GET: OfficeAssignments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var officeAssignment = await _assignmentService.GetById(id.Value);
+            if (officeAssignment == null)
+            {
+                return NotFound();
+            }
+            var officeAssignmentDTO = _mapper.Map<OfficeAssignmentDTO>(officeAssignment);
+
+            return View(officeAssignmentDTO);
+        }
+
+        // GET: OfficeAssignments/Create
+        public async Task<IActionResult> Create()
+        {
+            var listinstructors = await _instructorService.GetAll();
+            ViewBag.Instructors = listinstructors;
+            return View();
+        }
+
+        // POST: OfficeAssignments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(OfficeAssignmentDTO officeAssignment)
+        {
+            if (ModelState.IsValid)
+            {
+                var office = _mapper.Map<OfficeAssignment>(officeAssignment);
+                await _assignmentService.Insert(office);
+                return RedirectToAction(nameof(Index));
+            }
+            var listinstructors = _instructorService.GetAll();
+            ViewBag.Instructors = listinstructors;
+            return View(officeAssignment);
+        }
+
+        // GET: OfficeAssignments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var officeAssignment = await _assignmentService.GetById(id.Value);
+            if (officeAssignment == null)
+            {
+                return NotFound();
+            }
+            // var instructors = await _instructorService.GetById(id.Value);
+            ViewData["InstructorID"] = officeAssignment.InstructorID;
+            var officeAssignmentDTO = _mapper.Map<OfficeAssignmentDTO>(officeAssignment);
+            return View(officeAssignmentDTO);
+        }
+
+        // POST: OfficeAssignments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(OfficeAssignmentDTO officeAssignmentDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                var office = _mapper.Map<OfficeAssignment>(officeAssignmentDTO);
+                await _assignmentService.Update(office);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["InstructorID"] = officeAssignmentDTO.InstructorID;
+            return View(officeAssignmentDTO);
+        }
+
+        // GET: OfficeAssignments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var officeAssignment = await _assignmentService.GetById(id.Value);
+            if (officeAssignment == null)
+            {
+                return NotFound();
+            }
+            var officeAssignmentDTO = _mapper.Map<OfficeAssignmentDTO>(officeAssignment);
+            return View(officeAssignmentDTO);
+        }
+
+        // POST: OfficeAssignments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            try
+            {
+                await _assignmentService.Delete(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                ViewBag.Type = "danger";
+                return View("Delete");
+            }
+
+        }
+    }
+}
--- ContosoUniversity/Repositories/Implements/OfficeAssignmentRepository.cs
+++ ContosoUniversity/Repositories/Implements/OfficeAssignmentRepository.cs
@@ -1,5 +1,9 @@
 ﻿using ContosoUniversity.Data;
 using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Repositories.Implements
 {
@@ -10,5 +14,25 @@ namespace ContosoUniversity.Repositories.Implements
         {
             this.schoolContext = schoolContext;
         }
+
+        public new async Task<List<OfficeAssignment>> GetAll()
+        {
+            var listOfficeA = await schoolContext.OfficeAssignments.Include(o => o.Instructor).ToListAsync();   
+            return  listOfficeA;
+        }
+
+        //public new async Task Delete(int id)
+        //{
+        //    var entity = await GetById(id);
+
+        //    if (entity == null)
+        //        throw new System.Exception("The entity is null");
+        //    var flag = schoolContext.OfficeAssignments.Any(x => x.InstructorID == id);
+        //    if (flag)
+        //        throw new System.Exception("The Office have Instructors");
+
+        //    schoolContext.OfficeAssignments.Remove(entity);
+        //    await schoolContext.SaveChangesAsync();
+        //}
     }
 }
